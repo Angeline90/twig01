a9a07802cd76ad4cf9a5fa2e94fa89ce795808ed
Angeline90 <angeline.gauron@gmail.com>
2023-03-28 20:24:52 +0200

    add footer and details section

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+$author = 'Angie';
+$twig->addGlobal('author', $author);
+
+$contactEmail = 'contact@example.com';
+$twig->addGlobal('contactEmail', $contactEmail);
